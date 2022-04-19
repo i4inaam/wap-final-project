e0e79a793ad10151da6058c982bbbc64a989fcff
i4inaam <inaamulhaq32@yahoo.com>
2022-04-19 11:19:40 -0500

    qwert

--- client/scripts/login.js
+++ client/scripts/login.js
@@ -1,15 +1,39 @@
 
+// window.onload = function(){
+//     document.getElementById('loginBtn').onclick = function(){
+//     const uName = document.getElementById('uname').value;
+//     const pass = document.getElementById('password').value;
 
+//     if(uName == user.userName && pass == user.password){
+//         location.href = "homePage.html";
+//     }
+//     else{
+//         document.getElementById('eror').innerHTML = "User Name or Password is incorrect!"
+//     }
+// }
+// }
 window.onload = function(){
-    document.getElementById('loginBtn').onclick = function(){
-    const uName = document.getElementById('uname').value;
-    const pass = document.getElementById('password').value;
+    document.getElementById('loginBtn').onclick = async function () {
+        let result = await fetch('http://localhost:8000/users/', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify({
+               username: document.getElementById('uname').value,
+               password: document.getElementById('password').value
+            })
+        }).then(res => res.json());
+        console.log(result.username, result.password);
+        if(result.username === username && result.password === password){
+            window.location.href = "./client/homePage.html";
+        }
+        else{
+            document.getElementById('eror').innerHTML = "User Name or password is incorrect!";
+        }
+        
 
-    if(uName == user.userName && pass == user.password){
-        location.href = "homePage.html";
+        // document.getElementById('product-form').reset();
+        // renderBook(result);
     }
-    else{
-        document.getElementById('eror').innerHTML = "User Name or Password is incorrect!"
-    }
-}
 }
